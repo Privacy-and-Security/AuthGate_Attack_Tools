3f09163b811ae94a62f3218bd6be6c58daf12d70
Zhongyi-Lu <lu.zhon@northeastern.edu>
2023-11-06 21:41:18 -0800

    durable objects

--- src/worker.js
+++ src/worker.js
@@ -9,9 +9,27 @@
  */
 
 
-async function sendMultipleRequests({ targetUrl }) {
+async function sendMultipleRequestsSlowly({ targetUrl, requestTimes = 1 }) {
+  let url = new URL(targetUrl);
+
+  try {
+    for (let i = 0; i < requestTimes; i++) {
+      let response = await fetch(url);
+      if (response.status === 429) { // HTTP 429 Too Many Requests
+        return new Response(`Rate limit hit after ${i} requests`, { status: 200 });
+      }
+    }
+    return new Response(`No rate limit hit after ${requestTimes} requests`, { status: 200 });
+
+  } catch (error) {
+    return new Response(error.message, { status: 200 });
+  }
+}
+
+
+async function sendMultipleRequests({ targetUrl, requestTimes = 1 }) {
   let promises = [];
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < requestTimes; i++) {
     let promise = fetch(targetUrl).then(response => {
       if (response.status === 429) { // HTTP 429 Too Many Requests
         throw new Error(`Rate limit hit after ${i} requests`);
@@ -22,7 +40,7 @@ async function sendMultipleRequests({ targetUrl }) {
 
   try {
     await Promise.all(promises);
-    return new Response(`No rate limit hit after 100 requests`, { status: 200 });
+    return new Response(`No rate limit hit after ${requestTimes} requests`, { status: 200 });
   } catch (error) {
     return new Response(error.message, { status: 200 });
   }
@@ -30,6 +48,27 @@ async function sendMultipleRequests({ targetUrl }) {
 
 export default {
   async fetch(request, env, ctx) {
-    return await sendMultipleRequests({ targetUrl: "http://authgate.work" });
+    // Get the ID of the Durable Object.
+    const id = env.authgate.idFromName('Fetcher');
+
+    // Get the instance of the Durable Object.
+    const obj = env.authgate.get(id);
+
+    // Use the Durable Object.
+    const response = await obj.fetch(request);
+
+    return response;
   },
 };
+
+
+export class Fetcher {
+  constructor(state, env) {
+    this.state = state;
+  }
+
+  async fetch(request) {
+    // return new Response('Hello World');
+    return await sendMultipleRequestsSlowly({ targetUrl: 'http://authgate.work', requestTimes: 200 });
+  }
+}
