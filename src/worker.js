e21a245a0afab828674c2fbd7118fb0b9e66767d
Zhongyi-Lu <lu.zhon@northeastern.edu>
2023-11-06 21:01:21 -0800

    rate limit testing

--- src/worker.js
+++ src/worker.js
@@ -8,8 +8,28 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+async function sendMultipleRequests({ targetUrl }) {
+  let promises = [];
+  for (let i = 0; i < 100; i++) {
+    let promise = fetch(targetUrl).then(response => {
+      if (response.status === 429) { // HTTP 429 Too Many Requests
+        throw new Error(`Rate limit hit after ${i} requests`);
+      }
+    });
+    promises.push(promise);
+  }
+
+  try {
+    await Promise.all(promises);
+    return new Response(`No rate limit hit after 100 requests`, { status: 200 });
+  } catch (error) {
+    return new Response(error.message, { status: 200 });
+  }
+}
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello World!');
+    return await sendMultipleRequests({ targetUrl: "http://authgate.work" });
   },
 };
