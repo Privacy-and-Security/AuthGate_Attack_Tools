251492ea6968ff153e872ae0b22ff19e7369f170
Zhongyi-Lu <lu.zhon@northeastern.edu>
2023-11-19 17:23:38 -0800

    feat: enable batch requests

--- ddos-attack/src/worker.js
+++ ddos-attack/src/worker.js
@@ -10,29 +10,42 @@
 
 
 
-async function sendMultipleRequests({ targetUrl, requestTimes = 1 }) {
-  let promises = [];
-  const url = targetUrl;
+async function sendMultipleRequests({ targetUrl, requestTimes = 1, batchSize = 10 }) {
+  let successfulRequests = 0;
+  let allPromises = [];
 
   for (let i = 0; i < requestTimes; i++) {
-    let promise = (async function (index) {
-      const response = await fetch(url);
-      if (response.status === 429) {
-        throw new Error(`Rate limit hit after ${index + 1} requests`);
+    // Create a promise for the fetch request.
+    let promise = fetch(targetUrl)
+      .then(response => {
+        if (response.status === 429) {
+          throw new Error(`Rate limit hit after ${successfulRequests} requests`);
+        }
+        if (response.ok) {
+          successfulRequests++;
+        }
+        return response;
+      })
+      .catch(error => {
+        return `Request failed after ${successfulRequests} successful requests. Error: ${error.message}`;
+      });
+
+    allPromises.push(promise);
+
+    if ((i + 1) % batchSize === 0 || i === requestTimes - 1) {
+      try {
+        // Wait for the current batch to complete.
+        await Promise.all(allPromises);
+      } catch (error) {
+        return `Hit rate limit after ${successfulRequests} successful requests. Failed to send ${requestTimes - successfulRequests} requests`;
       }
-    })(i);
-
-    promises.push(promise);
+      allPromises = [];
+    }
   }
 
-  try {
-    await Promise.all(promises);
-    return `No rate limit hit after ${requestTimes} requests`;
-  } catch (error) {
-    return `Got error: ${error.message}`;
-
-  }
+  return `Finished sending requests with ${successfulRequests} successful requests. Failed to send ${requestTimes - successfulRequests} requests`;
 }
+
 
 export class Fetcher {
   constructor(state, env) {
@@ -40,10 +53,11 @@ export class Fetcher {
   }
 
   async fetch(request) {
-    const weak = await sendMultipleRequests({ targetUrl: 'https://apiweak.authgate.work/hello', requestTimes: 100 });
-    const fixed = await sendMultipleRequests({ targetUrl: 'https://api.authgate.work/', requestTimes: 100 });
+    const weak = await sendMultipleRequests({ targetUrl: 'https://apiweak.authgate.work/hello', requestTimes: 300 });
+    const fixed = await sendMultipleRequests({ targetUrl: 'https://api.authgate.work/', requestTimes: 300 });
+    // return new Response(`Fixed version: ${fixed}`, { status: 200 });
 
-    return new Response(`Weak version: ${weak},\nFixed version: ${fixed}`, { status: 200 });
+    return new Response(`Weak version: ${weak}\nFixed version: ${fixed}`, { status: 200 });
 
   };
 }
